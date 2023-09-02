14d68703752defb889148ba166ca0f35110708db
gajendra369 <gajendra.20cs028@sode-edu.in>
2023-09-02 22:04:55 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -30,13 +29,13 @@ class TicTacToe extends StatefulWidget {
 }
 
 class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
-  List<String> board = List.filled(9, '');//create list of length 9 all elements initialized to ''
+  List<String> board = List.filled(
+      9, ''); //create list of length 9 all elements initialized to ''
   bool isPlayer1Turn = true;
   String winner = '';
   late ImageProvider xImage;
   late ImageProvider oImage;
   final Map<int, AnimationController> _animationControllers = {};
-
 
   @override
   void initState() {
@@ -45,7 +44,7 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
     for (int i = 0; i < 9; i++) {
       _animationControllers[i] = AnimationController(
         vsync: this,
-        duration: const Duration(milliseconds: 500),//animation duration
+        duration: const Duration(milliseconds: 500), //animation duration
       );
       const interval = Interval(0.0, 0.5, curve: Curves.easeOut);
 
@@ -57,13 +56,13 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
     } // Adjust the duration as needed
   }
 
-
-  void _onTileTap(int index) { //callback for tap detection
+  void _onTileTap(int index) {
+    //callback for tap detection
     if (board[index] == '' && winner == '') {
       setState(() {
         board[index] = isPlayer1Turn ? 'X' : 'O';
         isPlayer1Turn = !isPlayer1Turn;
-        winner=_checkWinner();
+        winner = _checkWinner();
       });
     }
     final controller = _animationControllers[index];
@@ -71,12 +70,12 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
       _startPopAnimation(index);
     }
   }
+
   void _startPopAnimation(int index) {
     final controller = _animationControllers[index];
     controller?.reset();
     controller?.forward();
   }
-
 
   @override
   void dispose() {
@@ -87,31 +86,34 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
     super.dispose();
   }
 
-  void show(String msg){ //function for showing dailogbox
+  void show(String msg) {
+    //function for showing dailogbox
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Game Over'),
           content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
+            mainAxisSize: MainAxisSize.min,
+            children: [
               Text(msg, style: const TextStyle(fontSize: 20)),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              _restartGame();
-              Navigator.of(context).pop(); // Close the dialog
-            },
-            child: const Text('Restart Game'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  _restartGame();
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+                child: const Text('Restart Game'),
+              ),
+            ],
           ),
-          ],
-        ),
         );
       },
     );
   }
-  String _checkWinner() { //to determine the winner
+
+  String _checkWinner() {
+    //to determine the winner
     List<List<int>> winConditions = [
       [0, 1, 2],
       [3, 4, 5],
@@ -132,7 +134,6 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
       }
     }
 
-
     if (!board.contains('')) {
       show("draw match");
       return 'draw';
@@ -141,7 +142,8 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
     return '';
   }
 
-  void _restartGame() { //to reinitialize the conditions on game restart
+  void _restartGame() {
+    //to reinitialize the conditions on game restart
     setState(() {
       board = List.filled(9, '');
       isPlayer1Turn = true;
@@ -151,94 +153,106 @@ class _TicTacToeState extends State<TicTacToe> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return
-        Stack(
-          children:[
-            Image.asset(
-              'assets/bg.jpg',
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,
-            ),
-
-            SingleChildScrollView(
-            child: Center(
-              child: Column(
-                children: [
-
-                      Padding(
-                        padding: const EdgeInsets.only(left: 500.0),
-                        child: Row(
-                          children: [
-                            const Text("Turn :",style:TextStyle(fontSize: 40,color: Colors.white),),
-                            Image(
-                              image: AssetImage(isPlayer1Turn ? "assets/x_image.png" : "assets/o_image.png"),
-                              height: 30,
-                              width: 30,
-                            )
-
-                          ],
-                        ),
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/bg.jpg',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 500.0),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Turn :",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
                       ),
-                  const SizedBox(height: 20,),
-                  SizedBox(
-                    width: 600,
-                    height: 600,
-                    child: GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount( //defines size and layout of the grids
-                        crossAxisCount: 3,
-                      ),
-                      itemCount: 9,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () => _onTileTap(index),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white,width: 5),
-                            ),
-                            child: Center(
-                              child: board[index] == 'X'
-                                  ? AnimatedBuilder(
-                                animation: _animationControllers[index] ?? const AlwaysStoppedAnimation(0.0),
-                                builder: (context, child) {
-                                  return Transform.scale(
-                                    scale: (_animationControllers[index]?.value) ?? 0.0,
-                                    child: child,
-                                  );
-                                },
-                                child: Image.asset(
-                                  'assets/x_image.png',
-                                  width: 100,
-                                  height: 100,
-                                ),
-                              )
-                                  : board[index] == 'O'
-                                  ? AnimatedBuilder(
-                                animation: _animationControllers[index] ?? const AlwaysStoppedAnimation(0.0),
-                                builder: (context, child) {
-                                  return Transform.scale(
-                                    scale: (_animationControllers[index]?.value) ?? 0.0,
-                                    child: child,
-                                  );
-                                },
-                                child: Image.asset(
-                                  'assets/o_image.png',
-                                  width: 100,
-                                  height: 100,
-                                ),
-                              )
-                                  : null,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                      Image(
+                        image: AssetImage(isPlayer1Turn
+                            ? "assets/x_image.png"
+                            : "assets/o_image.png"),
+                        height: 30,
+                        width: 30,
+                      )
+                    ],
                   ),
-                ],
-              ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 600,
+                  height: 600,
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      //defines size and layout of the grids
+                      crossAxisCount: 3,
+                    ),
+                    itemCount: 9,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () => _onTileTap(index),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 5),
+                          ),
+                          child: Center(
+                            child: board[index] == 'X'
+                                ? AnimatedBuilder(
+                                    animation: _animationControllers[index] ??
+                                        const AlwaysStoppedAnimation(0.0),
+                                    builder: (context, child) {
+                                      return Transform.scale(
+                                        scale: (_animationControllers[index]
+                                                ?.value) ??
+                                            0.0,
+                                        child: child,
+                                      );
+                                    },
+                                    child: Image.asset(
+                                      'assets/x_image.png',
+                                      width: 100,
+                                      height: 100,
+                                    ),
+                                  )
+                                : board[index] == 'O'
+                                    ? AnimatedBuilder(
+                                        animation: _animationControllers[
+                                                index] ??
+                                            const AlwaysStoppedAnimation(0.0),
+                                        builder: (context, child) {
+                                          return Transform.scale(
+                                            scale: (_animationControllers[index]
+                                                    ?.value) ??
+                                                0.0,
+                                            child: child,
+                                          );
+                                        },
+                                        child: Image.asset(
+                                          'assets/o_image.png',
+                                          width: 100,
+                                          height: 100,
+                                        ),
+                                      )
+                                    : null,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),],
-        );
+          ),
+        ),
+      ],
+    );
   }
-
 }
